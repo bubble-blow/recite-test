4708873fc0abfa1614cf8089e01ad260a54b3e73
bubble-blow <3180481844@qq.com>
2026-04-05 10:46:39 +0800

    调整隐藏位样式：不再显示额外下划线

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function renderPassage() {
 
     if (state.hiddenSet.has(idx)) {
       span.classList.add('hidden-char');
-      span.textContent = '＿';
+      span.textContent = '　';
     } else {
       span.textContent = ch;
     }
